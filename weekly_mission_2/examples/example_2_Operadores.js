537f63a9cae693c785dd9fe56c7c3263bee5d263
Daniel Ramirez <contact@dann.dev>
2022-04-19 00:04:35 -0500

    add: Filter from array list with conditional

--- weekly_mission_2/examples/example_2_Operadores.js
+++ weekly_mission_2/examples/example_2_Operadores.js
@@ -59,3 +59,17 @@ const countriesEndsByia = countries.filter((country) => country.endsWith('ia'));
 console.log("Ejemplo 7: Paises que terminan en i");
 console.log(countriesEndsByia);
 
+// Ejemplo 8: Filtrar una lista por condicional
+console.log('\n--------------------------------------------------------------------------');
+const scores = [
+  { name: 'A', score: 95 },
+  { name: 'L', score: 98 },
+  { name: 'M', score: 80 },
+  { name: 'E', score: 50 },
+  { name: 'M', score: 85 },
+  { name: 'J', score: 100 },
+];
+
+const scoresGreaterEighty = scores.filter((score) => score.score > 80);
+console.log("Ejemplo 8: Filtrando elementos por score");
+console.log(scoresGreaterEighty);
