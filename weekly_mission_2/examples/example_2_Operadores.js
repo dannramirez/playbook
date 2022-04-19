aa7b1991c04509fb1f18275e0b79937a8a08917f
Daniel Ramirez <contact@dann.dev>
2022-04-18 23:34:47 -0500

    add: for each to calculate addition from array list

--- weekly_mission_2/examples/example_2_Operadores.js
+++ weekly_mission_2/examples/example_2_Operadores.js
@@ -3,3 +3,12 @@ console.log('\n-----------------------------------------------------------------
 const numbers = [1, 2, 3, 4, 5];
 console.log("Ejemplo 1: Imprimiendo los elementos de una lista");
 numbers.forEach(num => console.log(num));
+
+// Ejemplo 2: for Each para calcular la suma de todos los elementos de una lista
+console.log('\n--------------------------------------------------------------------------');
+let sum = 0;
+const numbers2 = [1, 2, 3, 4, 5];
+numbers2.forEach(num => sum += num);
+console.log("Ejemplo 2: Cálculo de la suma de los elementos de la lista");
+console.log(sum);
+
