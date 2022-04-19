239463ed1a9455a7a0a31937f92d6c168fd62dd0
Daniel Ramirez <contact@dann.dev>
2022-04-18 23:33:42 -0500

    add: for each array list

---
+++ weekly_mission_2/examples/example_2_Operadores.js
@@ -0,0 +1,5 @@
+// Ejemplo 1: for Each para imprimir elementos de una lista
+console.log('\n--------------------------------------------------------------------------');
+const numbers = [1, 2, 3, 4, 5];
+console.log("Ejemplo 1: Imprimiendo los elementos de una lista");
+numbers.forEach(num => console.log(num));
