3e3d92d99da761ee17fece2b24c82a39c9d4e1cd
Daniel Ramirez <contact@dann.dev>
2022-04-19 00:23:03 -0500

    add: areSomeTrue function return true or false if at least one item exists in the array

--- weekly_mission_2/examples/example_2_Operadores.js
+++ weekly_mission_2/examples/example_2_Operadores.js
@@ -112,3 +112,12 @@ const names13 = ['Explorer 1', 'Explorer 2', 'Explorer 3'];
 const result = names13.findIndex((name) => name.length > 7);
 console.log("Ejemplo 13: Primer elemento cuya palabra sea mayor a 7 esta en la posición "+ result);
 
+// Ejemplo 14: Uso de some, este método validará todos los elementos de la lista, y si alguno cumple con la validación indicada, regresará true, de lo contrario será false.
+console.log('\n--------------------------------------------------------------------------');
+
+// lista de elementos
+const bools = [true, true, false, true];
+// Uso de Some para ver si al menos uno de los elementos es false
+const areSomeTrue = bools.some((b) =>  b === false);
+console.log("Ejemplo 14: Alguno de los elementos en el array es false: " + areSomeTrue); //true
+
