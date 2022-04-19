e3c7322d8933863cf3ca5727e3062a91b487edfc
Daniel Ramirez <contact@dann.dev>
2022-04-18 23:47:27 -0500

    add: map function examples to lower and upper case

--- weekly_mission_2/examples/example_2_Operadores.js
+++ weekly_mission_2/examples/example_2_Operadores.js
@@ -15,7 +15,7 @@ console.log(sum);
 // Ejemplo 3: forEach para imprimir los países en letras mayúsculas
 console.log('\n--------------------------------------------------------------------------');
 const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland'];
-console.log("Ejemplo 5: Imprimiendo la lista de países en mayúsculas");
+console.log("Ejemplo 3: Imprimiendo la lista de países en mayúsculas");
 countries.forEach((element) => console.log(element.toUpperCase()));
 
 // Ejemplo 4: Uso de map para recorrer los elementos de una lista y crear una nueva lista
@@ -29,4 +29,21 @@ const numbersSquare = numbers4.map(function(num){ return num * num});
 //const numbersSquare = numbers4.map((num) => return num * num)
 console.log("Ejemplo 4: Imprimiendo la lista de elementos al cuadrado");
 console.log(numbersSquare);
+
+// Ejemplo 5: Uso de Map para convertir todos los nombres de una lista a minúsculas
+console.log('\n--------------------------------------------------------------------------');
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook'];
+const namesToUpperCase = names.map((name) => name.toLowerCase());
+
+console.log("Ejemplo 5: Uso de Map para convertir todos los nombres de una lista a minúsculas");
+console.log(namesToUpperCase);
+
+// Ejemplo 6: Uso de map para convertir todos los nombres de una lista a mayúsculas
+console.log('\n--------------------------------------------------------------------------');
+const countries6 = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland'];
+const countriesFirstThreeLetters = countries6.map((country) =>
+  country.toUpperCase().slice(0, 3) // el método slice obtiene solo la longitud marcada del string
+);
+console.log("Ejemplo 6: Uso de map para convertir todos los nombres de una lista a mayúsculas");
+console.log(countriesFirstThreeLetters);
 
