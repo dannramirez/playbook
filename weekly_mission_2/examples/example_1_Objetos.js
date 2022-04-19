1398605675fdb5906a15cc443f8b2cb9d358e53f
Daniel Ramirez <contact@dann.dev>
2022-04-18 23:19:05 -0500

    add: Object with properties

--- weekly_mission_2/examples/example_1_Objetos.js
+++ weekly_mission_2/examples/example_1_Objetos.js
@@ -5,3 +5,10 @@ const myObjetc = {} // Esto es un objeto vacío
 console.log("Ejemplo 1: Crear un objeto vacío")
 console.log(myObjetc)
 
+// Ejemplo 2: Crear un objeto con propiedades
+const myObjetc2 = {
+  name: "Dann",
+  age: 27
+}
+console.log("Ejemplo 2: Crear un objeto con propiedades")
+console.log(myObjetc2)
