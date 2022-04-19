dfec7403cb9d3ed17eb79e94b79782c71be388e8
Daniel Ramirez <contact@dann.dev>
2022-04-18 23:21:17 -0500

    add: Object with multiple properties

--- weekly_mission_2/examples/example_1_Objetos.js
+++ weekly_mission_2/examples/example_1_Objetos.js
@@ -7,8 +7,29 @@ console.log(myObjetc)
 
 // Ejemplo 2: Crear un objeto con propiedades
 const myObjetc2 = {
-  name: "Dann",
-  age: 27
+    name: "Dann",
+    age: 27
 }
 console.log("Ejemplo 2: Crear un objeto con propiedades")
 console.log(myObjetc2)
+
+
+// Ejemplo 3: Objeto con diferentes propiedades
+const myObject3 = {
+    name: "Gundus",
+    age: 3,
+    nicknames: [
+        "Gundus",
+        "Gundusaurio",
+        "Gundon"
+    ],
+    address: {
+        zip_code: "22686",
+        street: "Los Rayones",
+        city: "San Pedro"
+    }
+}
+console.log("Ejemplo 3: Objeto con diferentes propiedades")
+console.log(myObject3)
+console.log(myObject3.name)
+console.log(myObject3["address"])
